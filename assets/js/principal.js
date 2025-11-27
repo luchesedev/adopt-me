2a679fd0b7d8bfde33532b47d235daa0e4758d7f
unknown <pedroluchese@gmail.com>
2025-11-26 23:53:44 -0300

    inicio da tela animais com js da tela responsiva

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -7,3 +7,9 @@
     index = (index + 1) % imagens.length;
     imagens[index].classList.add('active');
   }, 3000);
+  const botaoMenu = document.getElementById('menu-toggle');
+const navItens = document.getElementById('nav-itens');
+
+botaoMenu.addEventListener('click', () => {
+  navItens.classList.toggle('mostrar');
+});
