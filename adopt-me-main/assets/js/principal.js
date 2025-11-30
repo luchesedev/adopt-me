88c9c7d689d1e529173c9383a672873c4bffe5d6
Felipe Diniz <163363555+fdinizS2@users.noreply.github.com>
2025-11-30 19:00:11 -0300

    Add files via upload

---
+++ adopt-me-main/assets/js/principal.js
@@ -0,0 +1,17 @@
+
+  let index = 0;
+  const imagens = document.querySelectorAll('.slider img');
+
+  setInterval(() => {
+    imagens[index].classList.remove('active');
+    index = (index + 1) % imagens.length;
+    imagens[index].classList.add('active');
+  }, 3000);
+
+const botaoMenu = document.getElementById('menu-toggle');
+const navItens = document.getElementById('nav-itens');
+
+botaoMenu.addEventListener('click', () => {
+  navItens.classList.toggle('mostrar');
+});
+
